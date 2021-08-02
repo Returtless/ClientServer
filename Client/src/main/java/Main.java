31fc58833f2557a56c9169654541177ccfc403d1
Vladislav Likhachev <mymindissharp@gmail.com>
2021-08-02 16:29:56 +0300

    Initial commit

---
+++ Client/src/main/java/Main.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Main {
+    public static String HOST = "localhost";
+    public static int PORT = 8080;
+
+    public static void main(String[] args) throws IOException {
+        try (Socket clientSocket = new Socket(HOST, PORT); PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true); BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+            out.println("Владислав");
+            String resp = in.readLine();
+            System.out.println(resp);
+        }
+    }
+}
